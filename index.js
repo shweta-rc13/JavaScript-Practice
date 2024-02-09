30c8ad05b6d5447f200bff9a5a68895f2842b987
shweta chaurasiya <shwetachaurasiya1313@gmail.com>
2024-02-09 21:39:08 +0530

    statement

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 
-var name= "shweta"
-console.log(name)
-var name = "Saurav"
-console.log(name)  output: - shweta , saurav
+// var name= "shweta"
+// console.log(name)
+// var name = "Saurav"
+// console.log(name)  output: - shweta , saurav
 
-let name = "Shweta";
-console.log(name);
-  name = "Saurav";
-console.log(name);
-name = "Shubham" ;
-console.log(name);
-output:-  Identifier 'name' has already been declared
+// let name = "Shweta";
+// console.log(name);
+//   name = "Saurav";
+// console.log(name);
+// name = "Shubham" ;
+// console.log(name);
+// output:-  Identifier 'name' has already been declared
 
-const age = 22;
-console.log(age);
-const age = 23;
-console.log(age);
-age = 23
-console.log(age)    //output:- Identifier 'age' has already been declared
+// const age = 22;
+// console.log(age);
+// const age = 23;
+// console.log(age);
+// age = 23
+// console.log(age)    //output:- Identifier 'age' has already been declared
 
-var var = 12;
-let let = 12;
-const const = 12;
+// var var = 12;
+// let let = 12;
+// const const = 12;
 
-var name= 'Shweta'
-var num = 123
-console.log(typeof(name));
-console.log(typeof(num));
+// var name= 'Shweta'
+// var num = 123
+// console.log(typeof(name));
+// console.log(typeof(num));
+
+
 
---
+++ statement.js
@@ -0,0 +1,7 @@
+let x, y, z;    
+x = 5;          
+y = 6;          
+z = x + y; 
+/*  1)A computer program is a list of "instructions" to be "executed" by a computer.
+2)In a programming language, these programming instructions are called statements.
+3)JavaScript statements are composed of: Values, Operators, Expressions, Keywords, and Comments.
