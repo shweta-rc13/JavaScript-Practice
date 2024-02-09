7519c8fa7713648279bd39a89a34c394c82cba05
shweta chaurasiya <shwetachaurasiya1313@gmail.com>
2024-02-09 21:48:33 +0530

    values

---
+++ values.js
@@ -0,0 +1,4 @@
+let numberLiteral = 42;      // Numeric literal
+let stringLiteral = "Hello"; // String literal
+let booleanLiteral = true;   // Boolean literal
+//Here, 42, "Hello", and true are literals.
