365b1679e3540290161427e262e4b5c32dd633c6
shweta chaurasiya <shwetachaurasiya1313@gmail.com>
2024-02-10 00:59:22 +0530

    variables

--- variable.js
+++ variable.js
@@ -2,10 +2,14 @@ var x = 5;
 var y = 6;
 var z = x + y;
 
-// let x = 5;
-// let y = 6;
-// let z = x + y;
+/*Variables defined with let can not be redeclared.
+ let x = 5;
+let y = 6;
+let z = x + y;
 
 // const x = 5;
 // const y = 6;
 // const z = x + y;
+const PI = 3.141592653589793;
+PI = 3.14;      // This will give an error
+PI = PI + 10;   // This will also give an error*/
